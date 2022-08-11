f3b1b33f0d4a9840e891915f3a79363ab4d189b8
Burak Saraloglu <buraksaraloglu1@gmail.com>
2022-08-12 00:57:13 +0300

    fix: update health endpoint

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,7 +5,7 @@ import movieRouter from './movie';
 
 const router = express.Router();
 
-router.use('v1/api/health', healthRouter);
+router.use('/', healthRouter);
 router.use('/movie', movieRouter);
 
 export default function initRouter(app: express.Application) {
